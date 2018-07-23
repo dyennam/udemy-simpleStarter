47bb0355c7b9cf0caf7ad4df17fa07a8975062ee
Dheeraj <dyennam@gmail.com>
2018-07-23 00:59:54 -0700

    removed template files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    return <div>Hello World</div>
+    return <div>Hello World test</div>
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
