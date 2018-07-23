a8c5cfa37fed7f0c2c518700170d1753b3cff2da
Dheeraj <dyennam@gmail.com>
2018-07-23 01:37:48 -0700

    Removed temp API key

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import API_KEYS from '../apiKeys';
 
-// const API_KEY = 'AIzaSyBN-yQQNYU6YVj4AYaQ2-1lWN7i75z2yQc';
 const API_KEY = API_KEYS.youtube_API_KEY;
 console.log('api key: ', API_KEY);
 
