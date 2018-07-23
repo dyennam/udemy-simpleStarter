68a7dd3bd6dc2ddab2eb76c7674f1ed10c8d1dad
Dheeraj <dyennam@gmail.com>
2018-07-23 01:13:45 -0700

    fixed eslint errors.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
 const App = () => {
     return <div>Hello World test</div>
-};
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const webpack = require('webpack');
+import webpack from 'webpack';
 
 module.exports = {
-	entry: ['./src/index.js'],
-	output: {
-		path: __dirname,
-		publicPath: '/',
-		filename: 'bundle.js',
-	},
-	module: {
-		loaders: [
-			{
-				exclude: /node_modules/,
-				loader: 'babel',
-				query: {
-					presets: ['react', 'es2015', 'stage-1'],
-				},
-			},
-		],
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx'],
-	},
-	devServer: {
-		historyApiFallback: true,
-		contentBase: './',
-		hot: true, // Hot reload setting
-		watchOptions: {
-			aggregateTimeout: 300,
-			poll: 1000,
-		},
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-	],
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1'],
+        },
+      },
+    ],
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true, // Hot reload setting
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
