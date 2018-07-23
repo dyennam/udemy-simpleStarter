b6e9fec7b6a4bb81fb7afcb35ace7dfc679a33aa
Dheeraj <dyennam@gmail.com>
2018-07-23 02:12:15 -0700

    Added JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import API_KEYS from '../apiKeys';
-import SearchBar from './components/search_bar'; 
+import SearchBar from './components/search_bar';
 
 const API_KEY = API_KEYS.youtube_API_KEY;
 
-
 const App = () => {
-    return <div>
-        Hello World
-        <SearchBar />
-        </div>
-}
+  return (
+    <div>
+      Hello:
+      <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
