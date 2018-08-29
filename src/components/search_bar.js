612e545f85e87f16b70fbcbe13b7844f5d0c848e
Dheeraj <dyennam@gmail.com>
2018-08-28 21:50:55 -0700

    commit for fields

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+
+    // this.onInputChange = this.onInputChange.bind(this);
+  }
+  
+
+  onInputChange(event, valueName) {
+    this.setState({
+      [valueName]: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-6'>
+            SearchBar:
+            <input
+              id='term'
+              onChange={event => this.onInputChange(event, 'term')}
+            />
+          </div>
+          <div>
+            Value of input: {this.state.term}
+          </div>
+        </div>
+      </div>
+    )
+  };
+}
 
 export default SearchBar;
